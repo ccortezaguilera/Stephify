78b208d2d69814cd956086043bea28518c05c553
Jonas Bauer <8391dppm@gmail.com>
2016-07-15 22:22:12 -0700

    I hope to fucking god this works

--- Sosos/ViewController.swift
+++ Sosos/ViewController.swift
@@ -1,15 +1,14 @@
 //
 //  ViewController.swift
-//  Sosos
+//  SOSOS
 //
-//  Created by Gonzalo Caballero on 7/15/16.
-//  Copyright © 2016 JTAC2. All rights reserved.
+//  Created by Jonas Bauer on 7/15/16.
+//  Copyright © 2016 Jonas Bauer. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, PBPebbleCentralDelegate {
     let defaults = NSUserDefaults.standardUserDefaults()
     var firstTime = false
     var name = ""
@@ -17,19 +16,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var egg: UIImageView!
     @IBOutlet weak var nameOfEgg: UILabel!
     
-    
+    var connectedWatch: PBWatch?
+    var watch: PBWatch?
+    var central = PBPebbleCentral.defaultCentral()
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         name = defaults.objectForKey("name")! as! String
         
         nameOfEgg.text! = name
+        // Set the delegate to receive PebbleKit events
+        if central.lastConnectedWatch() != nil{
+        watch = central.lastConnectedWatch()
+        self.central = PBPebbleCentral.defaultCentral()
+        self.central.delegate = self
+        // Register UUID
+        self.central.appUUID = PBSportsUUID
+        // Begin connection
+        self.central.run()
+        PBPebbleCentral.defaultCentral().delegate = self
+        self.watch!.appMessagesGetIsSupported({(watch: PBWatch, isAppMessagesSupported: Bool) -> Void in
+            if isAppMessagesSupported {
+                // Tell the user using the Label
+            }
+            else {
+            }
+        })
+        PBPebbleCentral.defaultCentral().run()
+        }
+        else{
+        }
     }
     
+    func pebbleCentral(central: PBPebbleCentral, watchDidConnect watch: PBWatch, isNew: Bool) {
+        print("Pebble connected: \(watch.name)")
+        // Keep a reference to this watch
+        self.connectedWatch = watch
+    }
+    
+    func pebbleCentral(central: PBPebbleCentral, watchDidDisconnect watch: PBWatch) {
+        print("Pebble disconnected: \(watch.name)")
+        // If this was the recently connected watch, forget it
+        if watch.isEqual(self.connectedWatch) {
+            self.connectedWatch = nil
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         //Is inverted because NSUserDefaults stars that way.
         firstTime = defaults.boolForKey("FirstTime")
@@ -39,6 +75,5 @@ class ViewController: UIViewController {
             print("Loading Welcome Screen")
         }
     }
-    
 }
 
